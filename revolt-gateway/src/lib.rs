33c835a324b5081e0ab1ec6d0a235dfb569b1de7
arslee07 <mail@arslee.me>
2023-03-09 23:39:21 +0900

    refactor!(gateway): overhaul

--- revolt-gateway/src/lib.rs
+++ revolt-gateway/src/lib.rs
@@ -1,15 +1,10 @@
-use futures_util::{SinkExt, Stream, StreamExt};
+use futures::{SinkExt, Stream, StreamExt};
 use revolt_models::event::{ClientToServerEvent, ServerToClientEvent};
-use std::time::Duration;
-use tokio::{
-    spawn,
-    sync::mpsc::{self, UnboundedReceiver, UnboundedSender},
-    time::sleep,
-};
-use tokio_tungstenite::tungstenite::Message;
+use tokio::net::TcpStream;
+use tokio_tungstenite::{tungstenite::Message, MaybeTlsStream, WebSocketStream};
 
 #[derive(Debug, thiserror::Error)]
-pub enum GatewayError {
+pub enum Error {
     #[error("Tungstenite error: {0}")]
     WsError(#[from] tokio_tungstenite::tungstenite::Error),
 
@@ -17,84 +12,58 @@ pub enum GatewayError {
     SerializationError(#[from] serde_json::Error),
 }
 
+/// A wrapper for Revolt WebSocket API
 #[derive(Debug)]
 pub struct RevoltWs {
-    server_event_receiver: UnboundedReceiver<Result<ServerToClientEvent, GatewayError>>,
-    client_event_sender: UnboundedSender<ClientToServerEvent>,
+    socket: WebSocketStream<MaybeTlsStream<TcpStream>>,
 }
 
 impl RevoltWs {
-    pub async fn connect() -> Result<RevoltWs, GatewayError> {
+    /// Connect to gateway with default Revolt WebSocket URL
+    pub async fn connect() -> Result<Self, Error> {
         RevoltWs::connect_with_url("wss://ws.revolt.chat".to_string()).await
     }
 
-    pub async fn connect_with_url(url: String) -> Result<Self, GatewayError> {
-        let (ws, _) = tokio_tungstenite::connect_async(url).await?;
-        let (mut ws_writer, mut ws_reader) = ws.split();
+    /// Connect to gateway with specified URL
+    pub async fn connect_with_url(url: String) -> Result<Self, Error> {
+        let (socket, _) = tokio_tungstenite::connect_async(url).await?;
 
-        let (server_sender, server_receiver) =
-            mpsc::unbounded_channel::<Result<ServerToClientEvent, GatewayError>>();
-        let (client_sender, mut client_receiver) = mpsc::unbounded_channel::<ClientToServerEvent>();
-
-        let c = RevoltWs {
-            server_event_receiver: server_receiver,
-            client_event_sender: client_sender.clone(),
-        };
-
-        // Receive message -> decode -> send to channel
-        spawn(async move {
-            while let Some(msg) = ws_reader.next().await {
-                // FIXME: what the fuck is this unreadable shit
-                let event = match msg {
-                    Ok(msg) => match msg.to_text() {
-                        Ok(text) => match serde_json::from_str::<ServerToClientEvent>(text) {
-                            Ok(res) => Ok(res),
-                            Err(err) => Err(GatewayError::from(err)),
-                        },
-                        Err(err) => Err(GatewayError::from(err)),
-                    },
-                    Err(err) => Err(GatewayError::from(err)),
-                };
-
-                server_sender.send(event).unwrap();
-            }
-        });
-
-        // Receive from channel -> encode -> send message
-        spawn(async move {
-            while let Some(event) = client_receiver.recv().await {
-                let msg = Message::Text(serde_json::to_string(&event).unwrap());
-                ws_writer.send(msg).await.unwrap();
-            }
-        });
-
-        // Send heartbeat periodically
-        spawn(async move {
-            loop {
-                client_sender
-                    .send(ClientToServerEvent::Ping { data: 0 })
-                    .unwrap();
-                sleep(Duration::from_secs(15)).await;
-            }
-        });
-
-        Ok(c)
+        Ok(RevoltWs { socket })
     }
 
-    pub async fn send(&mut self, event: ClientToServerEvent) -> Result<(), GatewayError> {
-        self.client_event_sender.send(event).unwrap();
+    /// Send an event to server
+    pub async fn send(&mut self, event: ClientToServerEvent) -> Result<(), Error> {
+        let msg = Self::encode_client_event(event)?;
+        self.socket.send(msg).await.map_err(Error::from)?;
 
         Ok(())
+    }
+
+    fn encode_client_event(event: ClientToServerEvent) -> Result<Message, Error> {
+        let json = serde_json::to_string(&event).map_err(Error::from)?;
+        let msg = Message::Text(json);
+
+        Ok(msg)
+    }
+
+    fn decode_server_event(msg: Message) -> Result<ServerToClientEvent, Error> {
+        let text = msg.to_text().map_err(Error::from)?;
+        let event = serde_json::from_str(text).map_err(Error::from)?;
+
+        Ok(event)
     }
 }
 
 impl Stream for RevoltWs {
-    type Item = Result<ServerToClientEvent, GatewayError>;
+    type Item = Result<ServerToClientEvent, Error>;
 
     fn poll_next(
         mut self: std::pin::Pin<&mut Self>,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Option<Self::Item>> {
-        self.server_event_receiver.poll_recv(cx)
+        self.socket
+            .poll_next_unpin(cx)
+            .map_ok(Self::decode_server_event)
+            .map_err(Error::from)?
     }
 }
