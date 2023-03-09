8d0550e12cf43b09d2c083c1079457b9a10f3228
arslee07 <mail@arslee.me>
2023-03-10 00:53:03 +0900

    chore(example): update ping-pong example

--- examples/ping-pong.rs
+++ examples/ping-pong.rs
@@ -1,7 +1,4 @@
-use std::{env, error::Error, sync::Arc};
-
-use futures_util::StreamExt;
-
+use futures::StreamExt;
 use revolt::{
     gateway::RevoltWs,
     http::RevoltHttp,
@@ -13,38 +10,63 @@ use revolt::{
     },
     util::extensions::BuilderExt,
 };
+use std::{env, error::Error, sync::Arc, time::Duration};
+use tokio::{spawn, sync::Mutex, time::sleep};
 
-// Context stores important things like revolt client or database connection
 struct Context {
     http: RevoltHttp,
+    ws: Arc<Mutex<RevoltWs>>,
 }
 
 type Result<T = ()> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
 #[tokio::main]
 async fn main() -> Result {
-    // Get token from environment variable
     let token = env::var("TOKEN")?;
 
-    // Create an http and ws instances
     let http = RevoltHttp::new(Authentication::BotToken(token.clone()));
-    let mut ws = RevoltWs::connect().await?;
+    let ws = RevoltWs::connect().await?;
+    let ws = Arc::new(Mutex::new(ws));
 
-    // Send authentication event to start receiving events
-    ws.send(ClientToServerEvent::Authenticate {
-        token: token.clone(),
-    })
-    .await?;
+    let ctx = Arc::new(Context { http, ws });
 
-    // Create the context
-    let ctx = Arc::new(Context { http });
+    authenticate(ctx.clone(), token.to_owned()).await?;
+    spawn_heartbeat(ctx.clone()).await?;
+    listen_to_events(ctx.clone()).await?;
 
-    // Wait for new event and then handle it
-    while let Some(event) = ws.next().await {
+    Ok(())
+}
+
+async fn authenticate(ctx: Arc<Context>, token: String) -> Result {
+    ctx.ws
+        .lock()
+        .await
+        .send(ClientToServerEvent::Authenticate { token })
+        .await?;
+    Ok(())
+}
+
+async fn spawn_heartbeat(ctx: Arc<Context>) -> Result {
+    let heartbeat_ws = Arc::clone(&ctx.ws);
+    spawn(async move {
+        loop {
+            heartbeat_ws
+                .lock()
+                .await
+                .send(ClientToServerEvent::Ping { data: 0 })
+                .await
+                .unwrap();
+            sleep(Duration::from_secs(15)).await;
+        }
+    });
+    Ok(())
+}
+
+async fn listen_to_events(ctx: Arc<Context>) -> Result {
+    while let Some(event) = ctx.ws.lock().await.next().await {
         let event = dbg!(event?);
         handle_event(event, Arc::clone(&ctx)).await?;
     }
-
     Ok(())
 }
 
@@ -66,6 +88,7 @@ async fn handle_message(message: Message, ctx: Arc<Context>) -> Result {
     if let Some(ref content) = message.content {
         match content.as_str() {
             "!ping" => ping(message, Arc::clone(&ctx)).await?,
+            "!pong" => pong(message, Arc::clone(&ctx)).await?,
             _ => {}
         };
     }
@@ -74,7 +97,14 @@ async fn handle_message(message: Message, ctx: Arc<Context>) -> Result {
 }
 
 async fn ping(message: Message, ctx: Arc<Context>) -> Result {
-    let payload = SendMessagePayload::builder().content("pong").build();
+    let payload = SendMessagePayload::builder().content("Pong!").build();
+    ctx.http.send_message(message.channel, payload).await?;
+
+    Ok(())
+}
+
+async fn pong(message: Message, ctx: Arc<Context>) -> Result {
+    let payload = SendMessagePayload::builder().content("Ping!").build();
     ctx.http.send_message(message.channel, payload).await?;
 
     Ok(())
