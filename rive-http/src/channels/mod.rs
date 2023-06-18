a86d8f200f084ae4ae90bfc5a6911d09f92141f0
arslee07 <mail@arslee.me>
2023-06-18 18:51:02 +0900

    feat(http): implement webhooks endpoints

--- rive-http/src/channels/mod.rs
+++ rive-http/src/channels/mod.rs
@@ -5,3 +5,4 @@ mod groups;
 mod interactions;
 mod messaging;
 mod voice;
+mod webhooks;
---
+++ rive-http/src/channels/webhooks.rs
@@ -0,0 +1,38 @@
+use rive_models::{payload::CreateWebhookPayload, webhook::Webhook};
+
+use crate::prelude::*;
+
+impl Client {
+    /// Create a webhook which 3rd party platforms can use to send messages.
+    pub async fn create_webhook(
+        &self,
+        channel_id: impl Into<String>,
+        payload: CreateWebhookPayload,
+    ) -> Result<Webhook> {
+        Ok(self
+            .client
+            .post(ep!(self, "/channels/{}/webhooks", channel_id.into()))
+            .json(&payload)
+            .auth(&self.authentication)
+            .send()
+            .await?
+            .process_error()
+            .await?
+            .json()
+            .await?)
+    }
+
+    /// Get all webhooks inside the channel.
+    pub async fn get_all_webhooks(&self, channel_id: impl Into<String>) -> Result<Vec<Webhook>> {
+        Ok(self
+            .client
+            .get(ep!(self, "/channels/{}/webhooks", channel_id.into()))
+            .auth(&self.authentication)
+            .send()
+            .await?
+            .process_error()
+            .await?
+            .json()
+            .await?)
+    }
+}
