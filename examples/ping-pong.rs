6797a76a334d7a9596568387202b6dbde80bc964
arslee07 <mail@arslee.me>
2023-06-21 21:10:33 +0900

    feat: shorthands

--- examples/ping-pong.rs
+++ examples/ping-pong.rs
@@ -1,68 +1,21 @@
-use futures::StreamExt;
-use rive::{
-    gateway::Gateway,
-    http::Client,
-    models::{
-        authentication::Authentication, data::SendMessageData, event::ServerEvent, message::Message,
-    },
-};
-use std::{env, error::Error};
-
-#[derive(Clone)]
-struct Context {
-    client: Client,
-    gateway: Gateway,
-}
-
-type Result<T = ()> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
+use rive::prelude::*;
 
 #[tokio::main]
-async fn main() -> Result {
-    let auth = Authentication::BotToken(env::var("TOKEN")?);
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let auth = Authentication::BotToken(std::env::var("TOKEN")?);
+    let mut rive = Rive::new(auth).await?;
 
-    let client = Client::new(auth.clone());
-    let gateway = Gateway::connect(auth.clone()).await?;
-
-    let ctx = Context { client, gateway };
-
-    while let Some(event) = ctx.gateway.clone().next().await {
-        handle_event(&event?, &ctx).await?;
-    }
-
-    Ok(())
-}
-
-async fn handle_event(event: &ServerEvent, ctx: &Context) -> Result {
-    match event {
-        ServerEvent::Message(message) => {
-            handle_message(&message, ctx).await?;
+    while let Some(event) = rive.gateway.next().await {
+        if let ServerEvent::Message(message) = event? {
+            if message.content.is_some_and(|c| c.starts_with("!ping")) {
+                let payload = SendMessageData {
+                    content: Some("Pong!".to_owned()),
+                    ..Default::default()
+                };
+                rive.http.send_message(message.channel, payload).await?;
+            };
         }
-        ServerEvent::Authenticated => {
-            println!("Client is authenticated");
-        }
-        _ => {}
-    };
-
-    Ok(())
-}
-
-async fn handle_message(message: &Message, ctx: &Context) -> Result {
-    if let Some(ref content) = message.content {
-        match content.as_str() {
-            "!ping" => ping(message, ctx).await?,
-            _ => {}
-        };
     }
-
-    Ok(())
-}
-
-async fn ping(message: &Message, ctx: &Context) -> Result {
-    let data = SendMessageData {
-        content: Some("Pong!".to_owned()),
-        ..Default::default()
-    };
-    ctx.client.send_message(&message.channel, data).await?;
 
     Ok(())
 }
--- rive/src/lib.rs
+++ rive/src/lib.rs
@@ -1,5 +1,9 @@
 #![doc = include_str!("../README.md")]
 
+use rive_gateway::Gateway;
+use rive_http::Client;
+use rive_models::authentication::Authentication;
+
 /// Revolt entities
 ///
 /// A re-export of [rive_models].
@@ -19,4 +23,38 @@ pub mod http {
 /// A re-export of [rive_gateway].
 pub mod gateway {
     pub use rive_gateway::*;
+}
+
+/// Re-export of everything
+pub mod prelude {
+    pub use rive_models::{
+        account::*, attachment::*, authentication::*, bot::*, channel::*, core::*, data::*,
+        embed::*, emoji::*, event::*, invite::*, member::*, message::*, mfa::*, onboarding::*,
+        permission::*, report::*, server::*, session::*, snapshot::*, stats::*, strike::*, user::*,
+        voice::*, webhook::*, *,
+    };
+
+    pub use rive_http::{Error as HttpError, Client};
+    pub use rive_gateway::{Error as GatewayError, Gateway, GatewayConfig};
+
+    pub use futures::StreamExt;
+    pub use crate::Rive;
+}
+
+#[derive(Debug, Clone)]
+pub struct Rive {
+    pub http: Client,
+    pub gateway: Gateway,
+}
+
+impl Rive {
+    /// Creates a new [`Rive`].
+    // TODO: make a separated error struct instead of gateway error exclusively?
+    // i mean that's kinda crappy isn't it? ------------>  VVVVVVVVVVVVVVVVVVV
+    pub async fn new(auth: Authentication) -> Result<Self, rive_gateway::Error> {
+        let http = Client::new(auth.clone());
+        let gateway = Gateway::connect(auth).await?;
+
+        Ok(Self { http, gateway })
+    }
 }
