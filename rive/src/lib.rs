9dcdd6e825379551eb79146da9b6340e411b21a7
arslee07 <mail@arslee.me>
2023-08-03 23:07:18 +0900

    feat(autumn): add Autumn client

--- rive/src/lib.rs
+++ rive/src/lib.rs
@@ -1,7 +1,6 @@
 #![doc = include_str!("../README.md")]
 
 use rive_gateway::Gateway;
-use rive_http::Client;
 use rive_models::authentication::Authentication;
 
 /// Revolt entities
@@ -25,13 +24,20 @@ pub mod gateway {
     pub use rive_gateway::*;
 }
 
+/// Client for the Autumn (Revolt file storage) API.
+///
+/// A re-export of [rive_autumn].
+pub mod autumn {
+    pub use rive_autumn::*;
+}
+
 /// Re-export of everything
 pub mod prelude {
     pub use rive_models::{
         account::*, attachment::*, authentication::*, bot::*, channel::*, core::*, data::*,
-        embed::*, emoji::*, event::*, invite::*, member::*, message::*, mfa::*, onboarding::*,
-        permission::*, report::*, server::*, session::*, snapshot::*, stats::*, strike::*, user::*,
-        voice::*, webhook::*, *,
+        embed::*, emoji::*, error::*, event::*, invite::*, member::*, message::*, mfa::*,
+        onboarding::*, permission::*, report::*, server::*, session::*, snapshot::*, stats::*,
+        strike::*, user::*, voice::*, webhook::*, *,
     };
 
     pub use rive_gateway::{Error as GatewayError, Gateway, GatewayConfig};
@@ -43,8 +49,9 @@ pub mod prelude {
 
 #[derive(Debug, Clone)]
 pub struct Rive {
-    pub http: Client,
+    pub http: rive_http::Client,
     pub gateway: Gateway,
+    pub autumn: rive_autumn::Client,
 }
 
 impl Rive {
@@ -52,9 +59,14 @@ impl Rive {
     // TODO: make a separated error struct instead of gateway error exclusively?
     // i mean that's kinda crappy isn't it? ------------>  VVVVVVVVVVVVVVVVVVV
     pub async fn new(auth: Authentication) -> Result<Self, rive_gateway::Error> {
-        let http = Client::new(auth.clone());
+        let http = rive_http::Client::new(auth.clone());
         let gateway = Gateway::connect(auth).await?;
+        let autumn = rive_autumn::Client::new();
 
-        Ok(Self { http, gateway })
+        Ok(Self {
+            http,
+            gateway,
+            autumn,
+        })
     }
 }
