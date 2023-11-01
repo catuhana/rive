37d99d6c1a9143cda01d4ddb59892f1f2caf5ce6
arslee07 <mail@arslee.me>
2023-11-01 19:59:55 +0900

    chore: update examples

--- examples/in-memory-cache.rs
+++ examples/in-memory-cache.rs
@@ -1,7 +1,6 @@
 //! This is an example of basic use of an in-memory cache.
 
-use futures::StreamExt;
-use std::{env, error::Error};
+use std::{env, error::Error, sync::Arc};
 
 use rive_cache_inmemory::InMemoryCache;
 use rive_gateway::Gateway;
@@ -10,53 +9,71 @@ use rive_models::{authentication::Authentication, data::SendMessageData, event::
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt::init();
+
     let auth = Authentication::BotToken(env::var("TOKEN")?);
 
     let http = Client::new(auth.clone());
-    let mut gateway = Gateway::connect(auth).await?;
+    let mut gateway = Gateway::new(auth);
 
     // Create a cache with default configuration
-    let cache = InMemoryCache::new();
+    let cache = Arc::new(InMemoryCache::new());
 
-    while let Some(event) = gateway.next().await {
-        let event = event?;
+    loop {
+        match gateway.next_event().await {
+            Ok(event) => {
+                tokio::spawn(handle_event(event, http.clone(), cache.clone()));
+            }
+            Err(err) => {
+                tracing::warn!("received an error: {:#?}", err);
+                break;
+            }
+        }
+    }
 
-        // Update the cache with the event
-        cache.update(&event);
+    Ok(())
+}
 
-        if let ServerEvent::Message(message) = event {
-            let Some(content) = message.content else {
-                continue;
+async fn handle_event(
+    event: ServerEvent,
+    http: rive_http::Client,
+    cache: Arc<InMemoryCache>,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    // Update the cache with the event
+    cache.update(&event);
+
+    if let ServerEvent::Message(message) = event {
+        let Some(ref content) = message.content else {
+            return Ok(());
+        };
+
+        // A command that send a number of messages in the cache.
+        if content.starts_with("!messages") {
+            let count = cache.stats().messages();
+
+            let data = SendMessageData {
+                content: Some(format!("Cached messages: {count}")),
+                ..Default::default()
+            };
+            http.send_message(&message.channel, &data).await?;
+        }
+        // A command that sends cached user info
+        else if content.starts_with("!user") {
+            let args = content.split_whitespace().collect::<Vec<&str>>();
+
+            let response = match args.get(1) {
+                Some(id) => match cache.user(&id.to_string().into()) {
+                    Some(user) => format!("```rust\n{:#?}\n```", user.value()),
+                    None => "User not found!".to_string(),
+                },
+                None => "No ID specified!".to_string(),
             };
 
-            // A command that send a number of messages in the cache.
-            if content.starts_with("!messages") {
-                let count = cache.stats().messages();
-
-                let data = SendMessageData {
-                    content: Some(format!("Cached messages: {count}")),
-                    ..Default::default()
-                };
-                http.send_message(&message.channel, &data).await?;
-            }
-            // A command that sends cached user info
-            else if content.starts_with("!user") {
-                let args = content.split_whitespace().collect::<Vec<&str>>();
-
-                let response = match args.get(1) {
-                    Some(id) => match cache.user(&id.to_string().into()) {
-                        Some(user) => format!("```rust\n{:#?}\n```", user.value()),
-                        None => "User not found!".to_string(),
-                    },
-                    None => "No ID specified!".to_string(),
-                };
-
-                let data = SendMessageData {
-                    content: Some(response),
-                    ..Default::default()
-                };
-                http.send_message(&message.channel, &data).await?;
-            }
+            let data = SendMessageData {
+                content: Some(response),
+                ..Default::default()
+            };
+            http.send_message(&message.channel, &data).await?;
         }
     }
 
--- examples/ping-pong.rs
+++ examples/ping-pong.rs
@@ -1,26 +1,43 @@
-use rive::prelude::*;
 use std::{env, error::Error};
+
+use rive_models::{authentication::Authentication, data::SendMessageData, event::ServerEvent};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    tracing_subscriber::fmt().init();
+
     let auth = Authentication::BotToken(env::var("TOKEN")?);
 
-    let mut rive = Rive::new(auth).await?;
+    let mut gateway = rive_gateway::Gateway::new(auth.clone());
+    let http = rive_http::Client::new(auth.clone());
 
-    while let Some(event) = rive.gateway.next().await {
-        let event = event?;
-
-        rive.update(&event);
-
-        if let ServerEvent::Message(message) = event {
-            if message.content.is_some_and(|c| c.starts_with("!ping")) {
-                let data = SendMessageData {
-                    content: Some("Pong!".to_owned()),
-                    ..Default::default()
-                };
-                rive.http.send_message(&message.channel, &data).await?;
-            };
+    loop {
+        match gateway.next_event().await {
+            Ok(event) => {
+                tokio::spawn(handle_event(event, http.clone()));
+            }
+            Err(err) => {
+                tracing::warn!("{err:#?}");
+                break;
+            }
         }
+    }
+
+    Ok(())
+}
+
+async fn handle_event(
+    event: ServerEvent,
+    http: rive_http::Client,
+) -> Result<(), Box<dyn Error + Sync + Send>> {
+    if let ServerEvent::Message(message) = event {
+        if message.content.is_some_and(|c| c.starts_with("!ping")) {
+            let data = SendMessageData {
+                content: Some("Pong!".to_owned()),
+                ..Default::default()
+            };
+            http.send_message(&message.channel, &data).await?;
+        };
     }
 
     Ok(())
